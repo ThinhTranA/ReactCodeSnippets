02d671a351144ba776e1f2026c41e703f9c146e9
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-10-21 21:54:08 +0800

    add coin flipper

---
+++ coin-flip/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CoinContainer from './CoinContainer'
+
+
+function App() {
+  return (
+    <div className="App">
+      <CoinContainer/>
+    </div>
+  );
+}
+
+export default App;
