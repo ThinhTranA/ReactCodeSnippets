0ca8c7c1ec6edd133d86d32ef9243c9fb434c58e
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-10-26 08:22:17 +0800

    create boxform component

--- color-box-maker/src/BoxList.js
+++ color-box-maker/src/BoxList.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import Box from "./Box";
+import NewBoxForm from './NewBoxForm'
 
 export default class BoxList extends Component {
     constructor(props){
         super(props);
         this.state = {boxes: [{width: 10, height: 40, color: "orange"}]};
+        this.create = this.create.bind(this);
+    }
+
+    create(newBox){
+        this.setState({
+            boxes: [...this.state.boxes, newBox]
+        })
     }
     
     render() {
@@ -17,6 +25,7 @@ export default class BoxList extends Component {
         return (
             <div>
                 <h1>Color Box Maker Thingy</h1>
+                <NewBoxForm createBox={this.create}/>
                 {boxes}
             </div>
         );
