5973e7c4abe4bcee064b29d7477690e333aaf78b
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-10-27 15:48:55 +0800

    Adding local spinner

--- dad-jokes/src/JokeList.js
+++ dad-jokes/src/JokeList.js
@@ -14,7 +14,8 @@ export default class JokeList extends Component {
         super(props)
 
         this.state = {
-            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
+            jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+            loading: false
         }
         this.handleClick = this.handleClick.bind(this);  
     }
@@ -41,6 +42,7 @@ export default class JokeList extends Component {
         }
         this.setState(st=> (
             {
+                loading: false,
                 jokes: [...st.jokes, ...jokes]
             }
         ));
@@ -63,10 +65,18 @@ export default class JokeList extends Component {
     }
 
     handleClick(){
-        this.getJokes();
+        this.setState({loading: true}, this.getJokes)
     }
 
     render() {
+        if(this.state.loading){
+            return (
+                <div className="JokeList-spinner">
+                    <i className="far fa-8x fa-laugh fa-spin"></i>
+                    <h1 className="JokeList-title">Loading...</h1>
+                </div>
+            )
+        }
         return (
             <div className="JokeList">
                 <div className="JokeList-sidebar">
