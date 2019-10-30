66ce1844aa0e11a28df0edc33cb5da824e23cd39
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-10-30 22:00:34 +0800

    cleanup and styling app

--- dog-app/src/DogDetails.js
+++ dog-app/src/DogDetails.js
@@ -6,8 +6,7 @@ export default class DogDetails extends Component {
     render() {
         let { dog } = this.props;
         return (
-            
-            <div className="container">
+
                 <div className="DogDetails row justify-content-center mt-5">
                     <div className="col-11 col-lg-5">
                         <div className="DogDetails-card card">
@@ -32,7 +31,6 @@ export default class DogDetails extends Component {
                     </div>
  
                 </div>
-            </div>
         )
     }
 }
