24add853b08c033e780e069200531385a7eaeb99
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-11-02 12:25:22 +0800

    Finish styling MiniPalette

--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -6,7 +6,7 @@ import { classes } from 'istanbul-lib-coverage';
 const styles = {
     root: {
         backgroundColor: "blue",
-        height: "100%",
+        height: "100vh",
         display: "flex",
         alignItems: "flex-start",
         justifyContent: "center"
