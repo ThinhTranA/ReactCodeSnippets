0793565f11117d9f59a8cb5a6661c470680957f2
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-11-06 18:25:45 +0800

    Add Todo Item Component

--- todos-hooks/src/TodoForm.js
+++ todos-hooks/src/TodoForm.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import { Paper, TextField } from '@material-ui/core'
-import useInputState from './hooks/userInputState' 
+import React from "react";
+import { Paper, TextField } from "@material-ui/core";
+import useInputState from "./hooks/userInputState";
 
-export default function TodoForm({addTodo}) {
-    const [value, handleChange, reset] = useInputState("");
-    return (
-        <Paper>
-            <form onSubmit={e => {
-                e.preventDefault();
-                addTodo(value);
-                reset();
-            }}>
-                <TextField value={value} onChange={handleChange}/>
-            </form>
-        </Paper>
-    )
+export default function TodoForm({ addTodo }) {
+  const [value, handleChange, reset] = useInputState("");
+  return (
+    <Paper style={{ margin: "1rem 0", padding: "0 1rem" }}>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          addTodo(value);
+          reset();
+        }}
+      >
+        <TextField
+          value={value}
+          onChange={handleChange}
+          margin="normal"
+          label="Add New Todo"
+        />
+      </form>
+    </Paper>
+  );
 }
